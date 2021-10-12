4bb4c677febbd1c2cbfe9bb05dbceff3decdde61
Daniil Oleshchuk <danieloleshuk@gmail.com>
2021-10-12 11:47:52 +0300

    Lab2: Added TextField, Text and Button to welcome user

--- iOS_Labs/ContentView.swift
+++ iOS_Labs/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fieldText = ""
+    @State var text = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onAppear {
-                print("Hello World!")
+        VStack {
+            TextField("Enter your name", text: $fieldText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Text(text).multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button {
+                if fieldText.count > 0 {
+                    text = "Hello " + fieldText + "!"
+                }
+            } label: {
+                Text("Say Hello")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue.cornerRadius(10))
+                    .foregroundColor(.white)
+                    .font(.headline)
             }
+        }.padding()
     }
 }
 
