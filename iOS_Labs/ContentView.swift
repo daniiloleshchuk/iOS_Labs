c9f44a75354bf7436cf72ae206e182c1e45b69d3
Daniil Oleshchuk <57315978+daniiloleshchuk@users.noreply.github.com>
2021-11-04 13:09:13 +0200

    Lab2: Added TextField, Text and Button to welcome user (#4)
    * Lab2: Added TextField, Text and Button to welcome user
    
    * Added sayHello func and aligned everything properly

--- iOS_Labs/ContentView.swift
+++ iOS_Labs/ContentView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var fieldText = ""
+    @State var text = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onAppear {
-                print("Hello World!")
+        VStack {
+            TextField("Enter your name", text: $fieldText, onCommit: {
+                sayHello()
+            })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Spacer()
+            
+            Text(text)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Button {
+                sayHello()
+            } label: {
+                Text("Say Hello")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue.cornerRadius(10))
+                    .foregroundColor(.white)
+                    .font(.headline)
             }
+        }.padding()
+    }
+    
+    func sayHello() {
+        if fieldText.count > 0 {
+            text = "Hello " + fieldText + "!"
+        }
     }
 }
 
